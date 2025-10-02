4839765c18f592f9f9bfdd85456a94dd2be5197c
Euan Goddard <euangoddard@manufacture2030.com>
2025-10-02 12:03:02 +0100

    fix: use correct original for static adapter

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://yoursite.qwik.dev",
+        origin: "https://i18n.euans.space",
       }),
     ],
   };
